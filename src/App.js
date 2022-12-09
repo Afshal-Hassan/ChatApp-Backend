7286ba2eae278bca925e4427205ad5c8bfe78299
Afshal Hassan <afshal.hassan@venturedive.com>
2022-12-10 04:43:59 +0500

    User and Friends API's added

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ const friendRouter = require("./routes/Friend");
 app.use("",userRouter);
 app.use("",friendRouter);
 
+app.get("/hello",(req,res)=>{
+    res.send("Hello");
+})
+
 
 const port=process.env.PORT || 3000;
 app.listen(port,()=>{
