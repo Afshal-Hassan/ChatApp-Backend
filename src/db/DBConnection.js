2334ba8febdd861f71366626cf006b26acea6664
Afshal Hassan <afshal.hassan@venturedive.com>
2022-12-10 07:06:12 +0500

    User and Friends API's added

--- src/db/DBConnection.js
+++ src/db/DBConnection.js
@@ -1,9 +1,9 @@
 const mysql=require("mysql");
 const mySqlConnnection = mysql.createConnection({
-    host:"localhost",
+    host:"127.0.0.1",
     port:"3306",
-    user:"root",
-    password:"root",
+    user:"afshal",
+    password:"afshal",
     database:"chatapp"
 });
 
